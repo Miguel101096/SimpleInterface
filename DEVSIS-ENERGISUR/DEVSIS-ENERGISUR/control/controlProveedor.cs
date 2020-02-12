1d6b02612320f171914b9a1afc5f9998f951e37b
Christian Naula <christian.alejandro98@hotmail.com>
2020-02-12 13:08:47 -0500

    proveedor

---
+++ DEVSIS-ENERGISUR/DEVSIS-ENERGISUR/control/controlProveedor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEVSIS_ENERGISUR.control
+{
+    class controlProveedor
+    {
+
+        private Conexion c;
+
+        public controlProveedor() { }
+
+        ~controlProveedor() { }
+
+        public void RregistrarProveedor(String RUC, String razons, String direcc, String correo, String telfconv, String telfcel)
+        {
+            try
+            {
+                this.c = new Conexion();
+                c.ejecutarSQL("EXECUTE Ingresar_Proveedor '" + RUC + "', '" + razons + "', '"
+                    + direcc + "', '" + correo + "', '" + telfconv + "', '" + telfcel + "'");
+                MessageBox.Show("Proveedor ingresado correctamente.");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error al ingresar nuevo proveedor: " + error);
+            }
+        }
+    }
+}
