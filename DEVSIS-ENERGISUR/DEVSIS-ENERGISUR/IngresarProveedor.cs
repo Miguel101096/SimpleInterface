e622d26a06d10db01eb270da1f00332224e9d7e4
Billy979818 <flaco_@hotmail.com.ar>
2020-02-14 09:55:01 -0500

    First commit Billy

--- DEVSIS-ENERGISUR/DEVSIS-ENERGISUR/IngresarProveedor.cs
+++ DEVSIS-ENERGISUR/DEVSIS-ENERGISUR/IngresarProveedor.cs
@@ -15,141 +15,19 @@ namespace DEVSIS_ENERGISUR
     public partial class IngresarProveedor : Form
     {
         private controlProveedor cp = new controlProveedor();
-        Validaciones v = new Validaciones();
+        static Validaciones v = new Validaciones();
+
 
         public IngresarProveedor()
         {
             InitializeComponent();
+
         }
 
-        public static bool validarNombre(String cadena)
-        {
-            String rx = "^[a-zA-Z_]+( [a-zA-Z_]+)*$";
-            if (Regex.IsMatch(cadena, rx))
-            {
-                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
 
-        public static bool validarDireccion(String cadena)
-        {
-            String rx = "^[a-zA-Z0-9_\\-\\,\\.]+( [a-zA-Z0-9_\\-\\.\\,]+)*$";
-            if (Regex.IsMatch(cadena, rx))
-            {
-                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
+        
 
-        public static bool validarEmail(String cadena)
-        {
-            String rx = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
-            if (Regex.IsMatch(cadena, rx))
-            {
-                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public static bool validarNumeros(String cadena)
-        {
-            String rx = "^[0-9]+$";
-            if (Regex.IsMatch(cadena, rx))
-            {
-                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public bool RucPersonaNatural(string ruc)
-        {
-            long isNumeric;
-            const int tamanoLongitudRuc = 13;
-            const string establecimiento = "001";
-            if (long.TryParse(ruc, out isNumeric) && ruc.Length.Equals(tamanoLongitudRuc))
-            {
-                var numeroProvincia = Convert.ToInt32(string.Concat(ruc[0] + string.Empty, ruc[1] + string.Empty));
-                var personaNatural = Convert.ToInt32(ruc[2] + string.Empty);
-                if ((numeroProvincia >= 1 && numeroProvincia <= 24) && (personaNatural >= 0 && personaNatural < 6))
-                {
-                    return ruc.Substring(10, 3) == establecimiento && VerificaCedula(ruc.Substring(0, 10));
-                }
-                return false;
-            }
-            return false;
-        }
-
-        public bool VerificaCedula(string ced)
-        {
-            int isNumeric;
-            var total = 0;
-            const int tamanoLongitudCedula = 10;
-            int[] coeficientes = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
-            const int numeroProvincia = 24;
-            const int tercerDigito = 6;
-
-            if (int.TryParse(ced, out isNumeric) && ced.Length == tamanoLongitudCedula)
-            {
-                var provincia = Convert.ToInt32(string.Concat(ced[0], ced[1], string.Empty));
-                var digitoTres = Convert.ToInt32(ced[2] + string.Empty);
-                if ((provincia > 0 && provincia <= numeroProvincia) && digitoTres < tercerDigito)
-                {
-                    var digitoVerificadorRecibido = Convert.ToInt32(ced[9] + string.Empty);
-                    for (var k = 0; k < coeficientes.Length; k++)
-                    {
-                        var valor = Convert.ToInt32(coeficientes[k] + string.Empty) * Convert.ToInt32(ced[k] + string.Empty);
-                        total = valor >= 10 ? total + (valor - 9) : total + valor;
-                    }
-                    var digitoVerificadorObtenido = total >= 10 ? (total % 10) != 0 ? 10 - (total % 10) : (total % 10) : total;
-                    return digitoVerificadorObtenido == digitoVerificadorRecibido;
-                }
-                return false;
-            }
-            return false;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+       private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -157,27 +35,32 @@ namespace DEVSIS_ENERGISUR
 
         private void textBoxRazonSocial_Leave(object sender, EventArgs e)
         {
-            if (validarNombre(textBoxRazonSocial.Text))
-            {
 
-            }
-            else if (textBoxRazonSocial.Text == String.Empty)
+            if (textBoxRazonSocial.Text == String.Empty)
             {
                 MessageBox.Show("Ingrese un valor para la entrada actual");
             }
-            else if (textBoxRazonSocial.TextLength > 50)
-            {
-                MessageBox.Show("La razón social excede el límite de cincuenta caracteres");
-            }
             else
             {
-                MessageBox.Show("Razón Social incorrecta");
+                if((textBoxRazonSocial.TextLength > 50))
+                {
+                    MessageBox.Show("La razón social excede el límite de cincuenta caracteres");
+                }
+                else
+                {
+                    v.validarNombre(textBoxRazonSocial.Text);
+                }
+                
             }
+
+            
+
+            
         }
 
         private void textMarca_Leave(object sender, EventArgs e)
         {
-            if (validarDireccion(textBoxDireccion.Text))
+            if (v.validarDireccion(textBoxDireccion.Text))
             {
 
             }
@@ -197,7 +80,7 @@ namespace DEVSIS_ENERGISUR
 
         private void textCorreo_Leave(object sender, EventArgs e)
         {
-            if (validarEmail(textBoxCorreo.Text))
+            if (v.validarEmail(textBoxCorreo.Text))
             {
 
             }
@@ -217,7 +100,7 @@ namespace DEVSIS_ENERGISUR
 
         private void textTelefonoConvencional_Leave(object sender, EventArgs e)
         {
-            if (validarNumeros(textBoxTelefonoConvencional.Text))
+            if (v.validarNumeros(textBoxTelefonoConvencional.Text))
             {
 
             }
@@ -237,7 +120,7 @@ namespace DEVSIS_ENERGISUR
 
         private void textTelefonoCelular_Leave(object sender, EventArgs e)
         {
-            if (validarNumeros(textBoxTelefonoCelular.Text))
+            if (v.validarNumeros(textBoxTelefonoCelular.Text))
             {
 
             }
@@ -257,7 +140,7 @@ namespace DEVSIS_ENERGISUR
 
         private void textRuc_Leave(object sender, EventArgs e)
         {
-            if (RucPersonaNatural(textBoxRuc.Text))
+            if (v.RucPersonaNatural(textBoxRuc.Text))
             {
 
             }
--- DEVSIS-ENERGISUR/DEVSIS-ENERGISUR/Validaciones.cs
+++ DEVSIS-ENERGISUR/DEVSIS-ENERGISUR/Validaciones.cs
@@ -58,6 +58,134 @@ namespace DEVSIS_ENERGISUR
 
             }
         }
-        
+
+        public bool RucPersonaNatural(string ruc)
+        {
+            long isNumeric;
+            const int tamanoLongitudRuc = 13;
+            const string establecimiento = "001";
+            if (long.TryParse(ruc, out isNumeric) && ruc.Length.Equals(tamanoLongitudRuc))
+            {
+                var numeroProvincia = Convert.ToInt32(string.Concat(ruc[0] + string.Empty, ruc[1] + string.Empty));
+                var personaNatural = Convert.ToInt32(ruc[2] + string.Empty);
+                if ((numeroProvincia >= 1 && numeroProvincia <= 24) && (personaNatural >= 0 && personaNatural < 6))
+                {
+                    return ruc.Substring(10, 3) == establecimiento && VerificaCedula(ruc.Substring(0, 10));
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public bool VerificaCedula(string ced)
+        {
+            int isNumeric;
+            var total = 0;
+            const int tamanoLongitudCedula = 10;
+            int[] coeficientes = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+            const int numeroProvincia = 24;
+            const int tercerDigito = 6;
+
+            if (int.TryParse(ced, out isNumeric) && ced.Length == tamanoLongitudCedula)
+            {
+                var provincia = Convert.ToInt32(string.Concat(ced[0], ced[1], string.Empty));
+                var digitoTres = Convert.ToInt32(ced[2] + string.Empty);
+                if ((provincia > 0 && provincia <= numeroProvincia) && digitoTres < tercerDigito)
+                {
+                    var digitoVerificadorRecibido = Convert.ToInt32(ced[9] + string.Empty);
+                    for (var k = 0; k < coeficientes.Length; k++)
+                    {
+                        var valor = Convert.ToInt32(coeficientes[k] + string.Empty) * Convert.ToInt32(ced[k] + string.Empty);
+                        total = valor >= 10 ? total + (valor - 9) : total + valor;
+                    }
+                    var digitoVerificadorObtenido = total >= 10 ? (total % 10) != 0 ? 10 - (total % 10) : (total % 10) : total;
+                    return digitoVerificadorObtenido == digitoVerificadorRecibido;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public  bool validarNumeros(String cadena)
+        {
+            String rx = "^[0-9]+$";
+            if (Regex.IsMatch(cadena, rx))
+            {
+                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public  bool validarEmail(String cadena)
+        {
+            String rx = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+            if (Regex.IsMatch(cadena, rx))
+            {
+                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public  bool validarDireccion(String cadena)
+        {
+            String rx = "^[a-zA-Z0-9_\\-\\,\\.]+( [a-zA-Z0-9_\\-\\.\\,]+)*$";
+            if (Regex.IsMatch(cadena, rx))
+            {
+                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool validarNombre(String cadena)
+        {
+            String rx = "^[a-zA-Z_]+( [a-zA-Z_]+)*$";
+            if (Regex.IsMatch(cadena, rx))
+            {
+                if (Regex.Replace(cadena, rx, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
